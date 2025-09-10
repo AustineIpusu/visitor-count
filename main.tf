87e1257f15ced36ea637323a3cb6057e281bcedf
AustineIpusu <holyaustine7@gmail.com>
2025-09-10 18:52:45 +0100

    main.tf configuration

--- main.tf
+++ main.tf
@@ -49,7 +49,6 @@ data "aws_iam_policy_document" "s3_bucket_policy" {
       type        = "Service"
       identifiers = ["cloudfront.amazonaws.com"]
     }
-
     actions = ["s3:GetObject"]
     resources = ["${aws_s3_bucket.my_website_bucket.arn}/*"]
 
